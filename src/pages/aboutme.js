14c66c7835d0fcacd75f5a880b3e2582f260e5f7
Jay-snow <marcusjsnow1@gmail.com>
2019-06-03 12:20:55 -0400

    aboutme.js now follows same pattern as other pages. Learned how to actually extend a react component it, instead of just using export default () => and writing directly to that.

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -1,10 +1,18 @@
 import React from "react"
+import Layout from "../components/layout"
 
 
 
+class AboutMe extends React.Component {
+render() {
 
-export default () => (
-    <div style={{color: `teal`}}>
+    return (
+    <div style={ {color: `teal`, margin:`3rem auto`, maxWidth: 600} }>
     <h1> Testing.</h1>
     </div>
-)
+    )
+  
+}
+}
+
+export default AboutMe
